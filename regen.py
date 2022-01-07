17c4d53d8b67bce81954a0ae09c5728e5a973939
surrealsocials <69706631+surrealsocials@users.noreply.github.com>
2022-01-08 05:24:07 +1100

    Add files via upload

---
+++ regen.py
@@ -0,0 +1,47 @@
+# Regen for cc
+
+def sum_of_digits(n):
+    sum=0
+    while (n>0):
+        t= n%10
+        sum= sum+t
+        n= n//10
+    return sum
+
+count = 0
+
+amount = input("how many cards do you want to generate?")
+n= input("Enter your credit card number: ")
+while count< int(amount):
+    try:
+        list_num= list(n)
+        if len(list_num)==16:
+            s1=0
+            s2= 0
+
+            for i in range(-1, -17,-1):     # Starting in reverse order
+                digit= list_num[i]
+                if i%2 == 0:                # Doubling every 2nd digit
+                    d= int(digit)*2
+                    if d>9:                 # Checking for 2-digit number
+                        s1= s1 + sum_of_digits(d)
+                    else:
+                        s1= s1 + d
+
+                else:
+                    s2= s2+ int(digit)
+
+            total= s1+s2
+
+
+            if total % 10 == 0:
+                count += 1
+                print (n)
+        else:
+            raise ValueError
+    except ValueError:
+        print("\nError!! Invalid input. Please enter correct value.")
+        print("\nPlease enter a 16-digit number only.")
+    n = int(n)+1
+    n = str(n)
+        
